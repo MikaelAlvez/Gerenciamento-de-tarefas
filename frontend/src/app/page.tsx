5d71ff4b702d5e21d8cf08facf076bb18b2ffcea
Mikael Alves <JOSE.ALVES08240@alunos.ufersa.edu.br>
2024-08-27 21:02:00 -0300

    feat: interface login

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,50 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import React, { useState } from 'react';
+import styles from './page.module.css';
+import Button from '@/app/Components/Button';
+import Link from 'next/link';
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebook } from "react-icons/fa";
 
-export default function Home() {
+export default function Login() {
+
   return (
-    <main className={styles.main}>
-      <h1>Login</h1>
-    </main>
+    <div className={styles.background}>
+      <div className={styles.loginContainer}>
+        <h2>Seja bem-vindo!</h2>
+        <h5 className={styles.iniContainer}>Por favor, insira seus dados.</h5>
+
+        <form>
+          <input 
+            type="email" 
+            placeholder="Email"
+            required
+          />
+          <input 
+            type="password" 
+            placeholder="Senha" 
+            required
+          />
+          <div className={styles.check}>
+            <div className={styles.checkbox}>
+              <input
+                type="checkbox"
+                placeholder='Lembre-se de mim'
+              ></input>
+              <label className={styles.textBlue}>Lembre-se de mim</label>
+            </div>
+            <label className={styles.textBlue}>Esqueceu a senha?</label>
+          </div>
+
+          <Button>Log in</Button>
+
+          <h4 className={styles.textConta}>Não tem uma conta? <Link className={styles.textBlue} href={'/Cadastrar'}>Inscreva-se</Link></h4>
+          <h4 className={styles.textConta}>Logar com</h4>
+          <div className={styles.redes}>
+            <FcGoogle size={'3em'}/>
+            <FaFacebook size={'3em'} style={{ color: '#3b5998' }}/>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
