b21fdfe50ce42ce03a64e3bd8af08587bff826f9
Mikael Alves <JOSE.ALVES08240@alunos.ufersa.edu.br>
2024-08-28 23:04:18 -0300

    feat: responsive source

--- frontend/src/pages/ListTasks/index.tsx
+++ frontend/src/pages/ListTasks/index.tsx
@@ -1,13 +1,24 @@
 import styles from './ListTasks.module.css'
 import TaskCards from '@/app/Components/TaskCards';
+import Source from '@/app/Components/Source';
+import Button from '@/app/Components/Button';
+import { IoMdAddCircle } from "react-icons/io";
+import { ImExit } from "react-icons/im";
 
 export default function ListTasks() {
   const tasks = [
+    {titleTask: 'Prova', description: 'Prova Programação II', status: 'Em progresso', date: '01 de setembro, 2024'},
+    {titleTask: 'Prova', description: 'Prova Programação II', status: 'Em progresso', date: '01 de setembro, 2024'},
     {titleTask: 'Prova', description: 'Prova Programação II', status: 'Em progresso', date: '01 de setembro, 2024'},
   ];
 
   return (
     <div className={styles.container}>
+      <div>
+        <label>Logout</label>
+        <ImExit/>
+      </div>
+      <Source></Source>
       {tasks.map((task, index) => (
         <TaskCards
           key={index}
@@ -17,6 +28,10 @@ export default function ListTasks() {
           date={task.date}
         />
       ))}
+      <Button> 
+        <IoMdAddCircle />
+        Nova tarefa
+      </Button>
     </div>
   );
 }
